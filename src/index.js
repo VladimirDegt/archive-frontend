d386d03c1f25a3c957fab6453969fcca5acf765f
Sasha <107486166+VladimirDegt@users.noreply.github.com>
2023-08-27 08:38:35 +0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/frontend-backend">
+        <BrowserRouter basename="/archive-frontend">
           <App />
         </BrowserRouter>
       </PersistGate>
