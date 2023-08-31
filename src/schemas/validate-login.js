d8b3a428825db1cbf44bc362d5b987f10f541aec
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-08-31 08:45:07 +0300

    refactoring validate

--- src/schemas/validate-login.js
+++ src/schemas/validate-login.js
@@ -2,13 +2,13 @@ import * as Yup from 'yup';
 
 export const SigninSchema = Yup.object().shape({
   password: Yup.string()
-     .min(2, 'Too Short!')
-     .max(30, 'Too Long!')
-     .required('Required'),
+     .min(2, 'Довжина повинна бути більше двох символів')
+     .max(30, 'Дуже багато символів')
+     .required("Поле обов'язкове до заповнення"),
    email: Yup
    .string()
-   .max(50, 'Too Long!')
-   .email('Invalid email')
-   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email format')
-   .required('Required'),
+   .max(50, 'Довжина повинна бути більше двох символів')
+   .email('Невірний формат email')
+   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Невірний формат email')
+   .required("Поле обов'язкове до заповнення"),
  });
--- src/schemas/validate-register.js
+++ src/schemas/validate-register.js
@@ -2,20 +2,20 @@ import * as Yup from 'yup';
 
 export const SignupSchema = Yup.object().shape({
   name: Yup.string()
-  .matches(/^[A-Za-zА-Яа-яІіЇїЄєҐґ']+$/, 'Name should only contain letters')
-     .min(2, 'Too Short!')
-     .max(30, 'Too Long!')
-     .required('Required'),
+  .matches(/^[A-Za-zА-Яа-яІіЇїЄєҐґ']+$/, "Ім'я повинно бути тільки з літер")
+     .min(2, 'Довжина повинна бути більше двох символів')
+     .max(30, 'Дуже багато символів')
+     .required("Поле обов'язкове до заповнення"),
   password: Yup.string()
-     .min(2, 'Too Short!')
-     .max(30, 'Too Long!')
-     .required('Required'),
+     .min(2, 'Довжина повинна бути більше двох символів')
+     .max(30, 'Дуже багато символів')
+     .required("Поле обов'язкове до заповнення"),
    email: Yup
    .string()
-   .max(50, 'Too Long!')
-   .email('Invalid email')
-   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email format')
-   .required('Required'),
+   .max(50, 'Довжина повинна бути більше двох символів')
+   .email('Невірний формат email')
+   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Невірний формат email')
+   .required("Поле обов'язкове до заповнення"),
  });
 
  
