9e4ad12a94b69ff2cfc0a687dee69ed0721cf1d1
VladimirDegt <degtyarevvladimirr@gmail.com>
2023-09-07 13:47:48 +0000

    Deploying to gh-pages from @ VladimirDegt/archive-frontend@d3cf2ceffb102d4acd073bbf6f6681abe1fca0b8 🚀

---
+++ static/js/256.1418cb36.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{9256:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("h3",{children:"Sorry, this page not found"})}}}]);
+//# sourceMappingURL=256.1418cb36.chunk.js.map
